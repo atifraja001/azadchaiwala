4d6cbe48a078958c5719d0ce284a6ca6ed755f46
awaisliaqat <awaisliaqat5@gmail.com>
2020-02-04 10:28:43 +0500

    content path update

--- App/Controllers/FetchDataController.php
+++ App/Controllers/FetchDataController.php
@@ -31,50 +31,52 @@ class FetchDataController
     {
 
     }
-    public function FetchImage(){
+
+    public function FetchImage()
+    {
         $image_id = clean_get('i');
         $image_type = clean_get('t');
         $filename = "assets/img/no_image.png"; // default filename
-        if(!empty($image_id) && !empty($image_type)){
-            if($image_type == "course"){
+        if (!empty($image_id) && !empty($image_type)) {
+            if ($image_type == "course") {
                 $course_image = new \App\Models\Courses();
                 $course_image = $course_image->getCourseImageById($image_id);
-                if($course_image) {
-                    $filename = "content/course_images/".$course_image['course_picture'];
+                if ($course_image) {
+                    $filename = $_SERVER['DOCUMENT_ROOT']."../content/course_images/" . $course_image['course_picture'];
                 }
-            }else if($image_type == "enroll"){
+            } else if ($image_type == "enroll") {
                 $enroll_image = new \App\Models\Enrollments();
                 $enroll_image = $enroll_image->getEnrollImageById($image_id);
-                if($enroll_image) {
-                    $filename = "content/receipt_images/" . $enroll_image['fee_receipt'];
+                if ($enroll_image) {
+                    $filename = "../content/receipt_images/" . $enroll_image['fee_receipt'];
                 }
-            }else if($image_type == "review"){
+            } else if ($image_type == "review") {
                 $review_image = new \App\Models\Reviews();
                 $review_image = $review_image->GetReviewImageById($image_id);
-                if($review_image) {
+                if ($review_image) {
                     $filename = "content/review_images/" . $review_image['person_image'];
                 }
-            }else if($image_type == "student"){
+            } else if ($image_type == "student") {
                 $student_image = new \App\Models\Students();
                 $student_image = $student_image->GetStudentImageById($image_id);
-                if($student_image) {
-                    $filename = "content/student_images/".$student_image['picture'];
+                if ($student_image) {
+                    $filename = "content/student_images/" . $student_image['picture'];
                 }
-            }else if($image_type == "teacher"){
+            } else if ($image_type == "teacher") {
                 $teacher_image = new \App\Models\Teachers();
                 $teacher_image = $teacher_image->GetTeacherImageById($image_id);
-                if($teacher_image) {
-                    $filename = "content/teacher_images/".$teacher_image['picture'];
+                if ($teacher_image) {
+                    $filename = "../content/teacher_images/" . $teacher_image['picture'];
                 }
-            }else if($image_type == "gallery"){
+            } else if ($image_type == "gallery") {
                 $gallery_image = new \App\Models\Gallery();
                 $gallery_image = $gallery_image->GetGalleryImageById($image_id);
-                if($gallery_image) {
-                    $filename = "content/gallery_images/".$gallery_image['image'];
+                if ($gallery_image) {
+                    $filename = "content/gallery_images/" . $gallery_image['image'];
                 }
             }
         }
-        if(!file_exists($filename)){
+        if (!file_exists($filename)) {
             $filename = "assets/img/no_image.png";
         }
         $handle = fopen($filename, "rb");
